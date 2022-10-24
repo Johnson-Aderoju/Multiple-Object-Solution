d7ab22d6a5bfdaa061eb8cae797665faa715e870
Johnson-Aderoju <aderenle4life@gmail.com>
2022-10-24 20:05:42 +0100

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,75 @@
+﻿public class Employee 
+{
+    public int empID;
+    public string empName;
+    public double salaryPerhour;
+    public int noOfWorkingHours;
+    public double netSalary;
+
+	public int getEmpID() {
+		return this.empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public string getEmpName() {
+		return this.empName;
+	}
+
+	public void setEmpName(string empName) {
+		this.empName = empName;
+	}
+
+	public double getSalaryPerhour() {
+		return this.salaryPerhour;
+	}
+
+	public void setSalaryPerhour(double salaryPerhour) {
+		this.salaryPerhour = salaryPerhour;
+	}
+
+	public int getNoOfWorkingHours() {
+		return this.noOfWorkingHours;
+	}
+
+	public void setNoOfWorkingHours(int noOfWorkingHours) {
+		this.noOfWorkingHours = noOfWorkingHours;
+	}
+
+    public static string organizationName = "Harsha Inc.";
+    public const string typeOfEmployee = "Contract Based";
+    public readonly string departmentName;
+    
+    public Employee()
+    {
+        departmentName = "Finance Department";
+    }
+    
+    public double NetSalary ()
+    {
+        netSalary = salaryPerhour*noOfWorkingHours;
+        return netSalary;
+    }
+
+	public void EmployeeCompany()
+	{
+		Console.WriteLine("*********"+organizationName+"********");
+		Console.WriteLine("Department: "+departmentName);
+		Console.WriteLine("Type of Employee: "+typeOfEmployee);
+		Console.WriteLine();
+	}
+
+    public void EmployeeDetails()
+    {
+       Console.WriteLine(getEmpID());
+	   Console.WriteLine(getEmpName());
+	   Console.WriteLine(getNoOfWorkingHours());
+	   Console.WriteLine(getSalaryPerhour());
+	   Console.WriteLine(NetSalary());
+    } 
+    
+
+
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,42 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        Employee employee1, employee2, employee3, employee4, 
+        employee5, employee6, employee7, employee8, employee9, employee10 ;
+
+        employee1 = new Employee();
+        employee2 = new Employee();
+        employee3 = new Employee();
+        employee4 = new Employee();
+        employee5 = new Employee();
+        employee6 = new Employee();
+        employee7 = new Employee();
+        employee8 = new Employee();
+        employee9 = new Employee();
+        employee10 = new Employee();
+        
+        employee1.setEmpID(001);
+        employee1.setEmpName("Bash");
+        employee1.setSalaryPerhour(800);
+        employee1.setNoOfWorkingHours(9);
+
+        employee1.EmployeeCompany();
+        employee1.EmployeeDetails();
+
+
+        
+        // Console.WriteLine("Employee ID "+ employee1.getEmpID());
+        // Console.WriteLine("Employee "+ employee1.getEmpID()+ " name is "+ employee1.getEmpName());
+        // Console.WriteLine("Employee"+ employee1.getEmpID()+ " salary per hour is "+employee1.getSalaryPerhour());
+        // Console.WriteLine("Employee "+ employee1.getEmpID()+ " number of working hour is "+employee1.getNoOfWorkingHours());
+        // Console.WriteLine("Employee "+ employee1.getEmpID()+ " net salary is "+employee1.NetSalary());
+        // Console.WriteLine("Employee "+ employee1.getEmpID()+ " works at "+Employee.organizationName);
+        // Console.WriteLine("Employee "+ employee1.getEmpID()+ " is a  "+Employee.typeOfEmployee);
+        // Console.WriteLine(employee1.departmentName);
+
+        string userInput = Console.ReadLine();
+        
+
+    }
+}
